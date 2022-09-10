c21510f9950bbdd775daec4cae7a600e940521d6
Daniel Goh <daniel.goh@kaligo.com>
2022-09-10 10:58:10 +0800

    Add features

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "products#index" # And remove the generated route `products/index`
+
+  resources :products, only: [:new, :create]
 end
